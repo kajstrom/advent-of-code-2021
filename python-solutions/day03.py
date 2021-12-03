0c85cf2c0e9d68efab61d9c84de72b3140bef2e8
Kaj Ström <kaj.strom@kapsi.fi>
2021-12-03 19:58:38 +0200

    Day 3, part 2

--- python-solutions/day03.py
+++ python-solutions/day03.py
@@ -32,6 +32,83 @@ def part1():
 
     print(f"Day 3, part 1: {int(gamma_rate_bits, 2) * int(epsilon_rate_bits, 2)}")
 
+def collect_pos_bits(bits, pos):
+    bits_in_pos = ""
+
+    for bit in bits:
+        bits_in_pos += bit[pos]
+
+    return bits_in_pos
+
+def filter_bits(bits, pos, bit_to_keep):
+    filtered_bits = []
+
+    for bit in bits:
+        if bit[pos] == bit_to_keep:
+            filtered_bits.append(bit)
+
+    return filtered_bits
 
 
-print(part1())
+def oxygen_rating(bits, pos, bit_len):
+    if pos == bit_len or len(bits) == 1:
+        return bits[0]
+
+    bits_in_pos = collect_pos_bits(bits, pos)
+
+    zeroes = bits_in_pos.count("0")
+    ones = bits_in_pos.count("1")
+
+    if zeroes > ones:
+        # keep only zeroes
+        return oxygen_rating(
+            filter_bits(bits, pos, "0"),
+            pos + 1,
+            bit_len
+        )
+    else:
+        # keep only ones
+        return oxygen_rating(
+            filter_bits(bits, pos, "1"),
+            pos + 1,
+            bit_len
+        )
+
+
+def co2_scrubber_rating(bits, pos, bit_len):
+    if pos == bit_len or len(bits) == 1:
+        return bits[0]
+
+    bits_in_pos = collect_pos_bits(bits, pos)
+
+    zeroes = bits_in_pos.count("0")
+    ones = bits_in_pos.count("1")
+
+    if zeroes > ones:
+        # keep only ones
+        return co2_scrubber_rating(
+            filter_bits(bits, pos, "1"),
+            pos + 1,
+            bit_len
+        )
+    else:
+        # keep only zeroes
+        return co2_scrubber_rating(
+            filter_bits(bits, pos, "0"),
+            pos + 1,
+            bit_len
+        )
+
+
+
+def part2():
+    bit_len = len(bits[0])
+
+    o_rating = oxygen_rating(bits, 0, bit_len)
+    co2_rating = co2_scrubber_rating(bits, 0, bit_len)
+
+    print(f"Day 3, part 2: {int(o_rating, 2) * int(co2_rating, 2)}")
+
+
+part1()
+part2()
