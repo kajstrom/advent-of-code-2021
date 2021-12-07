bc0d446484a47243f0be842ba299527538ff83ff
Kaj Ström <kaj.strom@kapsi.fi>
2021-12-07 18:11:09 +0200

    Day 7 refactor

--- python-solutions/day07.py
+++ python-solutions/day07.py
@@ -1,42 +1,33 @@
 import sys
 
 
-def read_input():
-    with open("inputs/day07.txt") as f:
-        return list(map((lambda x: int(x)), f.read().split(",")))
+def sum_diff(crabs, position):
+    return sum(map(lambda c: abs(c - position), crabs))
 
 
-def part1():
-    crabs = read_input()
+def sum_increasing_diff(crabs, position):
+    return sum(map(lambda c: sum(range(1, abs(c - position) + 1)), crabs))
 
+
+def calculate_cost_to_cheapest(cost_fn, crabs):
     min_crab = min(crabs)
     max_crab = max(crabs)
 
     cost = sys.maxsize
     for i in range(min_crab, max_crab + 1):
-        t_cost = sum(map(lambda c: abs(c - i), crabs))
+        t_cost = cost_fn(crabs, i)
 
         if t_cost < cost:
             cost = t_cost
 
-    print(f"Day 07, part 1: {cost}")
+    return cost
 
 
-def part2():
-    crabs = read_input()
+with open("inputs/day07.txt") as f:
+    crabs = list(map((lambda x: int(x)), f.read().split(",")))
 
-    min_crab = min(crabs)
-    max_crab = max(crabs)
+    cost_p1 = calculate_cost_to_cheapest(sum_diff, crabs)
+    print(f"Day 07, part 1: {cost_p1}")
 
-    cost = sys.maxsize
-    for i in range(min_crab, max_crab + 1):
-        t_cost = sum(map(lambda c: sum(range(1, abs(c - i) + 1)), crabs))
-
-        if t_cost < cost:
-            cost = t_cost
-
-    print(f"Day 07, part 2: {cost}")
-
-
-part1()
-part2()
+    cost_p2 = calculate_cost_to_cheapest(sum_increasing_diff, crabs)
+    print(f"Day 07, part 2: {cost_p2}")
